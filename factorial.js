09a07afad5884ac2b249ad99e1c4cd2ad264370e
Juliankix <ospinajulian2007@gmail.com>
2025-06-03 08:37:28 -0500

    Nivel 3 - Definido: Refactorizado el código usando función factorial para mejor organización y reutilización.

--- factorial.js
+++ factorial.js
@@ -1,10 +1,15 @@
-// Se agrega validación básica: que sea número entero positivo
+// Se crea una función para definir el proceso de cálculo.
+// Mejora la organización del código y su reutilización.
+
+function factorial(n) {
+  let f = 1;
+  for (let i = 1; i <= n; i++) f *= i;
+  return f;
+}
 
 let n = parseInt(prompt("Número:"));
 if (isNaN(n) || n < 0) {
   console.log("Número inválido");
 } else {
-  let f = 1;
-  for (let i = 1; i <= n; i++) f *= i;
-  console.log(f);
+  console.log(factorial(n));
 }
